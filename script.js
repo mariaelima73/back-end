798b8ee4ba3599c3f127c04fc334c6bf7ae3541a
maria.e.lima73 <maria.e.lima73@aluno.senai.br>
2024-06-06 10:44:54 -0300

    Dia 3

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-
+document.addEventListener("DOMContentLoaded", function() {
 let entrar = document.getElementById('entrar')
 let recuperar = document.getElementById('recuperar')
 let registrar = document.getElementById('registrar')
+let concluir = document.getElementById('concluir')
 let login = document.getElementById('login')
 let voltar = document.getElementById('voltar')
 let sobre = document.getElementById('sobre')
 
-document.addEventListener("DOMContentLoaded", function() {
-voltar.addEventListener('click', function () {
-    location.href = 'index.html'
-})
-})
-
-document.addEventListener("DOMContentLoaded", function() {
-sobre.addEventListener('click', function() {
-    location.href = 'sobre.html'
-})
-
-login.addEventListener('click', function () {
-    location.href = 'login.html'
-})
-})
-
-entrar.addEventListener('click', function () {
-    location.href = 'sistema.html'
-})
-recuperar.addEventListener('click', function () {
+if (entrar) {
+entrar.addEventListener('click', function(){
+    location.href = 'sisfsgpe.html'
+})}
+if (recuperar){
+recuperar.addEventListener('click', function(){
     location.href = 'recuperar.html'
-})
-registrar.addEventListener('click', function () {
+})}
+if (registrar){
+registrar.addEventListener('click', function(){
     location.href = 'registro.html'
+})}
+if (concluir){
+concluir.addEventListener('click', function(){
+    location.href = 'login.html'
+})}
+if (login){
+login.addEventListener('click', function(){
+    location.href = 'login.html'
+})}
+if (voltar){
+voltar.addEventListener('click', function(){
+    location.href = 'index.html'
+})}
+if (sobre){
+sobre.addEventListener('click', function(){
+    location.href = 'sobre.html'
+})}
 })
+
